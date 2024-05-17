ae63dc4f256fea951d4a7f93d42871828e1dca9e
Erick Matsen <ematsen@gmail.com>
2024-05-17 03:59:59 -0700

    cleaning up notebooks

--- shmex/shm_data.py
+++ shmex/shm_data.py
@@ -7,7 +7,6 @@ from netam import framework
 
 dataset_dict = {
     "shmoof": "data/v0/shmoof_pcp_2023-11-30_MASKED.csv.gz",
-    #"tangshm": "data/tang-deepshm_size2_edges_22-May-2023.branch_length.csv.gz",
     "tangshm": "data/v1/tang-deepshm-oof_pcp_2024-04-09_MASKED_NI.csv.gz",
     "cui": "data/v0/cui-et-al-oof_pcp_2024-02-22_MASKED_NI.csv.gz",
     "cuims": "data/v0/cui-et-al-oof-msproc_pcp_2024-02-29_MASKED_NI.csv",
