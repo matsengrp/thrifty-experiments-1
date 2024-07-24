8e8acd759715bd09dd56d64f1e51b6e4d514621b
Erick Matsen <ematsen@gmail.com>
2024-07-24 00:27:58 -0700

    going back to picking device

--- shmex/shm_zoo.py
+++ shmex/shm_zoo.py
@@ -22,9 +22,9 @@ from shmex.shm_data import train_val_dfs_of_nicknames
 
 site_count = 500
 epochs = 1000
-# device = pick_device()
-print("Using CPU")
-device = "cpu"
+device = pick_device()
+# print("Using CPU")
+# device = "cpu"
 
 model_parameters = {
     "sml": {
