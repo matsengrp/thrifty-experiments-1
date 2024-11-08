570508d59b890022a2ff1bb9c4442b96df28c1fc
Erick Matsen <ematsen@gmail.com>
2024-11-08 04:30:44 -0800

    modified:   install_models.sh

--- train/install_models.sh
+++ train/install_models.sh
@@ -13,8 +13,6 @@ file_pairs=(
 for pair in "${file_pairs[@]}"; do
     src_base=$(echo $pair | awk '{print $1}')
     dest_base=$(echo $pair | awk '{print $2}')
-    cp "${src_base}.pth" "$BASE_DEST_DIR/${dest_base}.pth"
-    echo "Copied ${src_base}.pth to $BASE_DEST_DIR/${dest_base}.pth"
-    cp "${src_base}.yml" "$BASE_DEST_DIR/${dest_base}.yml"
-    echo "Copied ${src_base}.yml to $BASE_DEST_DIR/${dest_base}.yml"
+    cp "trained_models/${src_base}.pth" "$BASE_DEST_DIR/${dest_base}.pth"
+    cp "trained_models/${src_base}.yml" "$BASE_DEST_DIR/${dest_base}.yml"
 done
