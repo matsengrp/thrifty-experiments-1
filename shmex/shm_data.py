2aec2560defe79eb7720ad619c91cd2f18413b01
Erick Matsen <ematsen@gmail.com>
2024-12-02 02:23:02 -0800

    gzip file for val_curated..

--- shmex/shm_data.py
+++ shmex/shm_data.py
@@ -8,7 +8,7 @@ from netam import framework
 
 dataset_dict = {
     "shmoof": "data/v0/shmoof_pcp_2023-11-30_MASKED.csv.gz",
-    "val_curatedShmoofNotbigNoN": "data/v0/shmoof_pcp_2023-11-30_notbig_NI_noN.csv",
+    "val_curatedShmoofNotbigNoN": "data/v0/shmoof_pcp_2023-11-30_notbig_NI_noN.csv.gz",
     "tangshm": "data/v1/tang-deepshm-oof_pcp_2024-04-09_MASKED_NI.csv.gz",
     "syn10x": "data/v1/wyatt-10x-1p5m_fs-all_pcp_2024-04-29_NI_SYN.csv.gz",
     "v1wyatt": "data/v1/wyatt-10x-1p5m_fs-all_pcp_2024-04-29_NI_noN_no-naive.csv.gz",
